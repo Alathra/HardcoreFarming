0562a50095f2c059b25da7643711eeb6a830ae65
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2023-12-16 22:51:19 +0100

    fix: corrected feedAmount

--- HardcoreFarming/src/main/java/me/ShermansWorld/HardcoreFarming/listeners/ItemUseOnEntityListener.java
+++ HardcoreFarming/src/main/java/me/ShermansWorld/HardcoreFarming/listeners/ItemUseOnEntityListener.java
@@ -129,11 +129,11 @@ public class ItemUseOnEntityListener implements Listener {
 				} else {
 					if (e.getHand() == EquipmentSlot.HAND) {
 						p.getInventory().setItem(EquipmentSlot.HAND, new ItemStack(Material.AIR));;
-						item.setAmount(item.getAmount() - feedAmount);
+						item.setAmount(item.getAmount() - (feedAmount - 1));
 						p.getInventory().addItem(item);
 					} else {
 						p.getInventory().setItem(EquipmentSlot.OFF_HAND, new ItemStack(Material.AIR));;
-						item.setAmount(item.getAmount() - feedAmount);
+						item.setAmount(item.getAmount() - (feedAmount -1));
 						p.getInventory().setItem(EquipmentSlot.OFF_HAND, item);
 					}
 				}
